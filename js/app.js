d535be0498b9dc9a6b120f244a15ec09a7a1da86
Matthew Langford <m.c.langford@live.com>
2017-03-07 22:36:15 -0700

    route updates and triangle pictures added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,33 @@
-angular.module('myApp', []);/**
- * Created by Flarebrass on 3/6/2017.
- */
+angular.module('myApp', ['ui.router'])
+
+.config(function ($urlRouterProvider, $stateProvider) {
+
+    $urlRouterProvider.when('', '/home');
+
+    $stateProvider
+        .state('home',{
+            templateUrl: 'views/home.html',
+            url: '/home'
+        })
+        .state('land',{
+            templateUrl: 'views/landingView.html',
+            url: '/land'
+        })
+        .state('friendsView',{
+            templateUrl: 'views/friendsView.html',
+            url: '/friends'
+        })
+        .state('friendProfile',{
+            templateUrl: 'views/friendProfileView.html',
+            url: '/friendPro'
+        })
+        .state('search',{
+            templateUrl: 'views/searchView.html',
+            url: 'search'
+        })
+        .state('update',{
+            templateUrl: 'views/updateView.html',
+            url: 'update'
+        })
+
+});
