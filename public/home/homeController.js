50f0d2a83f9be1a3932493dfae34fcdc369e143f
Matthew Langford <m.c.langford@live.com>
2017-03-27 05:57:26 -0600

    minor changes

--- public/home/homeController.js
+++ public/home/homeController.js
@@ -1,5 +1,7 @@
 angular.module('friends').controller('homeController', function ($scope, landingService) {
 
-    $scope.storeProfile = landingService.storeProfile;
+    $scope.storeProfile = (user)=>{
+        landingService.storeProfile(user);
+    }
 
 });
